635c0b199027618969814aba68ee19425acb8524
Marco <Marco@Marco-Portatile>
2014-11-11 22:20:25 +0100

    Persona

---
+++ ORegenWS/src/com/ge/oilandgas/oregen/Persona.java
@@ -0,0 +1,24 @@
+package com.ge.oilandgas.oregen;
+
+public class Persona {
+
+	
+	private String nome;
+	private String cognome;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	
+	
+}
