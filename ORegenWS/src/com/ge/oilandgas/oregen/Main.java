a4992261215ad658f372f2c42cab41aad40fa49e
Marco <Marco@Marco-Portatile>
2014-11-11 23:04:48 +0100

    aaaaaaaaaaa

--- ORegenWS/src/com/ge/oilandgas/oregen/Main.java
+++ ORegenWS/src/com/ge/oilandgas/oregen/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println("ciao");
 		System.out.println("ciao");
 		//ciao
+		//prova
 		System.out.println("ciao");
 		System.out.println("provaoriva");
 	}
--- ORegenWS/src/com/ge/oilandgas/oregen/Persona.java
+++ ORegenWS/src/com/ge/oilandgas/oregen/Persona.java
@@ -26,4 +26,7 @@ public class Persona {
 		this.eta = eta;
 	}
 	
+	
+	//dio
+	
 }
